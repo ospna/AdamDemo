870b0da79b75ce3f9f530d3264c30cb3cd307c5d
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2018-11-18 19:10:24 -0500

    Deleted the unnecessary files and added the idle animation to our boy Adam

---
+++ AdamDemo/Assets/_scripts/SwitchAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchAnimation : MonoBehaviour {
+
+
+	public Animator anim;
+
+	void Awake()
+	{
+		anim = this.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.anyKey) {
+			anim.SetInteger("walk",1);
+		} else {
+			anim.SetInteger("walk",0);
+		}
+
+		/*  This is how you would do it with Triggers
+			you need to remove the Integer trigger to make this work
+
+		if (Input.anyKey){
+			anim.SetTrigger("walking");
+			anim.ResetTrigger("stopWalking");
+		} else {
+			anim.SetTrigger("stopWalking");
+			anim.ResetTrigger("walking");
+		}
+		*/
+	}
+}
